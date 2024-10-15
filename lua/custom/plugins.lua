aff5201508c06d43c5410c9639da358b8ff54b91
achyutem <achyutem.ad@gmail.com>
2024-10-15 17:22:02 +0530

    added react snippets

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -106,5 +106,21 @@ local plugins = {
       require "custom.configs.lspconfig"
     end,
   },
+  -- Add nvim-cmp configuration here
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "mlaursen/vim-react-snippets",
+    },
+    opts = function()
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+      
+      require("vim-react-snippets").lazy_load()
+      
+      local cmp = require("cmp")
+    end,
+  },
 }
 return plugins
